4467f7d0b5f128f211782fa8b50ab163a4486c8d
Mishrahariom <hariom.cs2020@mpgi.edu.in>
2024-07-13 17:01:03 +0530

    Changes the some data

--- src/components/pricing/Faq.jsx
+++ src/components/pricing/Faq.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react"
+import React, { useState } from "react"
 import { faq } from "../../dummydata"
 import Heading from "../common/heading/Heading"
 
@@ -14,12 +14,12 @@ const Faq = () => {
 
   return (
     <>
-      <Heading subtitle='FAQS' title='Frequesntly Ask Question' />
+      <Heading subtitle='FAQS' title='Frequently Asked Questions' />
       <section className='faq'>
         <div className='container'>
           {faq.map((val, index) => (
-            <div className='box'>
-              <button className='accordion' onClick={() => toggle(index)} key={index}>
+            <div className='box' key={index}>
+              <button className='accordion' onClick={() => toggle(index)}>
                 <h2>{val.title}</h2>
                 <span>{click === index ? <i className='fa fa-chevron-down'></i> : <i className='fa fa-chevron-right'></i>}</span>
               </button>
@@ -37,3 +37,46 @@ const Faq = () => {
 }
 
 export default Faq
+
+
+
+
+// import React, { useRef, useState } from "react"
+// import { faq } from "../../dummydata"
+// import Heading from "../common/heading/Heading"
+
+// const Faq = () => {
+//   const [click, setClick] = useState(false)
+
+//   const toggle = (index) => {
+//     if (click === index) {
+//       return setClick(null)
+//     }
+//     setClick(index)
+//   }
+
+//   return (
+//     <>
+//       <Heading subtitle='FAQS' title='Frequesntly Ask Question' />
+//       <section className='faq'>
+//         <div className='container'>
+//           {faq.map((val, index) => (
+//             <div className='box'>
+//               <button className='accordion' onClick={() => toggle(index)} key={index}>
+//                 <h2>{val.title}</h2>
+//                 <span>{click === index ? <i className='fa fa-chevron-down'></i> : <i className='fa fa-chevron-right'></i>}</span>
+//               </button>
+//               {click === index ? (
+//                 <div className='text'>
+//                   <p>{val.desc}</p>
+//                 </div>
+//               ) : null}
+//             </div>
+//           ))}
+//         </div>
+//       </section>
+//     </>
+//   )
+// }
+
+// export default Faq
